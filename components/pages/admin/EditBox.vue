39c11d8bfb377307fbe7cb4c72b8a75aea8ae23a
Rechzz <reshmareghu.ptf@gmail.com>
2024-12-13 11:18:25 +0530

    "Removed unnecessary comments from EditBox.vue template and script setup."

--- components/pages/admin/EditBox.vue
+++ components/pages/admin/EditBox.vue
@@ -22,8 +22,7 @@
 </template>
 
 <script setup>
-//this is a sample commit
-//this commit is not to be cherry picked
+
 const emits = defineEmits(['edit','cancel'])
 const props = defineProps({
     items: Object
