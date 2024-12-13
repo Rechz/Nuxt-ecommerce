aae1002574334ebc4df2a8ecf12d45772415b19b
Rechzz <reshmareghu.ptf@gmail.com>
2024-12-13 11:12:27 +0530

    refactor: added quill

--- app.vue
+++ app.vue
@@ -1,11 +1,21 @@
 <template>
   <div>
-    <NuxtLayout>
-      <NuxtPage></NuxtPage>
-    </NuxtLayout>
+
+      <!-- <NuxtPage></NuxtPage> -->
+
+        <quill-editor ref="editor" theme="snow" v-model:content="htmlMessage" toolbar="essential"
+          content-type="html"></quill-editor>
+          <div v-html="htmlMessage"></div>
+
+
   </div>
+  <input type="date" placeholder="YYYY-MM-DD">
 </template>
 
+<script setup>
+// import { ClientOnly } from '#build/components';
+const htmlMessage = ref('');
+</script>
 <style>
 *{
   margin: 0;
